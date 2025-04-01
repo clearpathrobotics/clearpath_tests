6a0e8504780eb80f826106c8f1ebf64368cc284e
Chris Iverach-Brereton <civerachb@clearpathrobotics.com>
2025-04-01 11:11:31 -0400

    Ensure the firmware is 2.3 or higher to allow fan control; otherwise just skip the fan test

--- clearpath_tests/clearpath_tests/fan_test.py
+++ clearpath_tests/clearpath_tests/fan_test.py
@@ -28,11 +28,12 @@
 # POSSIBILITY OF SUCH DAMAGE.
 
 from clearpath_generator_common.common import BaseGenerator
-from clearpath_platform_msgs.msg import Fans
+from clearpath_platform_msgs.msg import Fans, Status
 from clearpath_tests.test_node import ClearpathTestNode, ClearpathTestResult
 
 import rclpy
-from rclpy.qos import qos_profile_system_default
+from rclpy.duration import Duration
+from rclpy.qos import qos_profile_sensor_data, qos_profile_system_default
 
 
 class FanTestNode(ClearpathTestNode):
@@ -56,10 +57,56 @@ class FanTestNode(ClearpathTestNode):
         # Publish the message
         self.publisher.publish(msg)
 
+    def check_firmware_version(self):
+        """
+        Check the firmware version; it must be 2.3+ for this test to work
+
+        :return: A tuple of the form (version, sufficient)
+        """
+        self.mcu_status = None
+
+        def mcu_callback(status):
+            self.mcu_status = status
+
+        mcu_sub = self.create_subscription(
+            Status,
+            f'/{self.namespace}/platform/mcu/status',
+            mcu_callback,
+            qos_profile_sensor_data,
+        )
+
+        start_at = self.get_clock().now()
+        timeout_duration = Duration(seconds=10)
+        while self.get_clock().now() - start_at <= timeout_duration and self.mcu_status is None:
+            rclpy.spin_once(self)
+        mcu_sub.destroy()
+
+        if self.mcu_status is None:
+            return (None, False)
+        else:
+            (major, minor, _) = self.mcu_status.firmware_version.split('.')
+            major = int(major)
+            minor = int(minor)
+
+            ok = (
+                major > 2
+                or (
+                    major == 2
+                    and minor >= 3
+                )
+            )
+
+            return (
+                self.mcu_status.firmware_version,
+                ok
+            )
+
     def start(self):
         self.publish_timer = self.create_timer(1 / self.publish_rate, self.publish_callback)
 
     def run_test(self):
+        results = []
+
         # kick out if the lights are in an uncontrolled state
         user_input = self.promptYN("Are all e-stops cleared, the robot's battery charged, and the front lights white & rear lights red?")  # noqa: E501
         if user_input == 'N':
@@ -69,9 +116,22 @@ class FanTestNode(ClearpathTestNode):
                 'Robot in error state; cannot control fans',
             )]
 
-        self.start()
+        # kick out if the firmware version is too low
+        (version, version_ok) = self.check_firmware_version()
+        if not version_ok:
+            return [ClearpathTestResult(
+                None,  # Skip; don't treat as failure
+                'Fans (firmware)',
+                f'Firmware v{version} is insufficient to run fan tests',
+            )]
+        else:
+            results.append(ClearpathTestResult(
+                True,
+                'Fans (firmware)',
+                f'Firmware {version} is sufficient for fan control',
+            ))
 
-        results = []
+        self.start()
 
         msg = Fans()
         for _ in range(self.n_fans):
@@ -80,9 +140,9 @@ class FanTestNode(ClearpathTestNode):
         self.publisher.publish(msg)
         user_input = self.promptYN('Are all fans stopped?')
         if user_input == 'Y':
-            results.append(ClearpathTestResult(True, 'All fans off', None))
+            results.append(ClearpathTestResult(True, 'Fans (all off)', None))
         else:
-            results.append(ClearpathTestResult(False, 'All fans off', None))
+            results.append(ClearpathTestResult(False, 'Fans (all off)', None))
 
         for i in range(self.n_fans):
             for j in range(self.n_fans):
@@ -92,9 +152,9 @@ class FanTestNode(ClearpathTestNode):
 
             user_input = self.promptYN(f'Is ONLY fan {i+1} on?')
             if user_input == 'Y':
-                results.append(ClearpathTestResult(True, f'Fan {i+1} only', None))
+                results.append(ClearpathTestResult(True, f'Fans ({i+1} only)', None))
             else:
-                results.append(ClearpathTestResult(False, f'Fan {i+1} only', None))
+                results.append(ClearpathTestResult(False, f'Fans ({i+1} only)', None))
 
         for _ in range(self.n_fans):
             msg.fans[i] = 255
@@ -102,9 +162,9 @@ class FanTestNode(ClearpathTestNode):
 
         user_input = self.promptYN('Are all fans running?')
         if user_input == 'Y':
-            results.append(ClearpathTestResult(True, 'All fans on', None))
+            results.append(ClearpathTestResult(True, 'Fans (all on)', None))
         else:
-            results.append(ClearpathTestResult(False, 'All fans on', None))
+            results.append(ClearpathTestResult(False, 'Fans (all on)', None))
 
         return results
 
