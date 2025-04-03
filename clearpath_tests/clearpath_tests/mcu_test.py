f2c4f052d70f456b4d26b38aa07f3348a60db366
Chris Iverach-Brereton <civerachb@clearpathrobotics.com>
2025-04-03 14:22:51 -0400

    Fix serial MCU tests

--- clearpath_tests/clearpath_tests/mcu_test.py
+++ clearpath_tests/clearpath_tests/mcu_test.py
@@ -164,7 +164,7 @@ class McuTestNode(ClearpathTestNode):
 
         @return True if the handle exists, otherwise False
         """
-        return os.path.is_file(device)
+        return os.path.isfile(device)
 
     def check_serial_permissions(self, device):
         """
@@ -174,7 +174,7 @@ class McuTestNode(ClearpathTestNode):
 
         @return True if we have RW permissions, otherwise False
         """
-        exists = os.path.is_file(device)
+        exists = os.path.isfile(device)
         perms_ok = False
 
         if exists:
